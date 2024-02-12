649818f8a5ba664beb99ff846aec5e906150eacb
HazeNoquera <noquera.hazelann.bscs2021@gmail.com>
2024-02-12 23:21:09 +0800

    Log in

--- T-sync/routes/web.php
+++ T-sync/routes/web.php
@@ -14,9 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
-});
-
-Route::get('/login', function () {
-    return view('login');
+    return view('welcome');
 });
